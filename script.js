ee021c8d8d5abf346ccf1c0c71b54f6dc12de8db
Paradox Shadow <testa7294@gmail.com>
2025-01-15 00:31:58 -0800

    Added the event listeners -When the share button is clicked -When the window resizes.

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-const share = document.getElementById('share');
-const avatarContainer = document.querySelector('.avatar-container');
 const footerContainer = document.querySelector('.footer-container');
-const shareContainer = document.querySelector('.share-container ');
-share.addEventListener('click', () => {
+const avatarContainer = document.querySelector('.avatar-container');
+const shareContainer = document.querySelector('.share-container');
+const share = document.getElementById('share');
+
+
+//Initially hide the share container
+shareContainer.classList.add('hidden');
+
+share.addEventListener('click', (e) => {
+    e.currentTarget.classList.toggle('clicked');
     if (window.innerWidth < 768 ) {
-        if (footerContainer.classList.contains('light')) {
-            //Change the color of footer container
-            footerContainer.classList.toggle('light');
-            footerContainer.classList.add('dark');
-    
-            //Make the avatar go away
-            avatarContainer.style.display = 'none';
-    
-            //Make the share icons visible
-            shareContainer.classList.toggle('hidden');
-    
+        avatarContainer.classList.toggle('hidden');
+        shareContainer.classList.toggle('hidden');
+        footerContainer.classList.toggle('dark');
+        
         } 
     else if (window.innerWidth >= 768) {
-        
+        shareContainer.classList.toggle('hidden');
     }    
-    }
+    
    
+});
+
+//To make sure visibility of the containers are toggled when the window is resized
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        if (share.classList.contains('clicked')) {
+            avatarContainer.classList.add('hidden');
+            shareContainer.classList.remove('hidden');
+            footerContainer.classList.add('dark');
+        } else {
+            avatarContainer.classList.remove('hidden');
+            shareContainer.classList.add('hidden');
+            footerContainer.classList.remove('dark');
+        }
+    } else {
+        avatarContainer.classList.remove('hidden');
+        footerContainer.classList.remove('dark');
+        if (share.classList.contains('clicked')) {
+            shareContainer.classList.remove('hidden');
+        } else {
+            shareContainer.classList.add('hidden');
+        }
+    }
 });
